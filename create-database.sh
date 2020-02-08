6165af992dca3932a1b5fb3837bc5589afe3b119
logan <yuelong.chen.btr@gmail.com>
2020-02-08 12:09:39 +0800

    [M]: gene biotype and only using 1-22 XY chromosome

--- create-database.sh
+++ create-database.sh
@@ -90,7 +90,7 @@ cp ${GRCH37_DICT} ${rRNA_INTERVAL_LIST}
 grep 'gene_biotype "rRNA"'  ${GRCH37_GTF_FILE} | awk '$3 == "transcript"' | cut -f1,4,5,7,9 | \
     perl -lane '
         /transcript_id "([^"]+)"/ or die "no transcript_id on $.";
-        print join "\t", (@F[0,1,2,3], $1)
+        print join "\t", (@F[0,1,2,3], $1) if $F[0]=~/^[XY0-9]/;
     ' | \
     sort -k1V -k2n -k3n >> ${rRNA_INTERVAL_LIST}
 
@@ -147,10 +147,10 @@ cp ${GRCH38_DICT} ${rRNA_INTERVAL_LIST}
 
 
 # Intervals for rRNA transcripts.
-grep 'gene_type "rRNA"'  ${GRCH38_GTF_FILE} | awk '$3 == "transcript"' | cut -f1,4,5,7,9 | \
+grep 'gene_biotype "rRNA"'  ${GRCH38_GTF_FILE} | awk '$3 == "transcript"' | cut -f1,4,5,7,9 | \
     perl -lane '
         /transcript_id "([^"]+)"/ or die "no transcript_id on $.";
-        print join "\t", (@F[0,1,2,3], $1)
+        print join "\t", (@F[0,1,2,3], $1) if $F[0]=~/^[XY0-9]/;
     ' | \
     sort -k1V -k2n -k3n >> ${rRNA_INTERVAL_LIST}
 
