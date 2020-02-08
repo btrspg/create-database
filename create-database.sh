7b28b7ab9d51adcc4dc052d429dc5c2908e26153
logan <yuelong.chen.btr@gmail.com>
2020-02-08 11:53:51 +0800

    [M]: gene_type in grch37 was gene_biotype

--- create-database.sh
+++ create-database.sh
@@ -87,7 +87,7 @@ cp ${GRCH37_DICT} ${rRNA_INTERVAL_LIST}
 
 
 # Intervals for rRNA transcripts.
-grep 'gene_type "rRNA"'  ${GRCH37_GTF_FILE} | awk '$3 == "transcript"' | cut -f1,4,5,7,9 | \
+grep 'gene_biotype "rRNA"'  ${GRCH37_GTF_FILE} | awk '$3 == "transcript"' | cut -f1,4,5,7,9 | \
     perl -lane '
         /transcript_id "([^"]+)"/ or die "no transcript_id on $.";
         print join "\t", (@F[0,1,2,3], $1)
